e425a772e93387775ec4842e4df38695dac187fc
MCRoj <marcorojas86@gmail.com>
2020-02-02 12:32:17 -0700

    first upload

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // LateUpdate is called once per frame and is guaranteed to run after all items have been processed in Update
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private float convertedX;
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+
+        GetComponent<ChuckSubInstance>().RunCode(@"
+            SinOsc foo => dac;
+            repeat(12)
+            {
+            Math.random2f(440, 660) => foo.freq;
+            25::ms => now; 
+            440 => foo.freq;
+            25::ms => now;
+            }        
+        ");
+    }
+
+    
+    //called before rendering a frame.  where most game code goes.
+    void Update()
+    {
+        convertedX = (int) transform.position.x + 9.5f;
+        GetComponent<ChuckSubInstance>().RunCode(string.Format(@"
+            TriOsc foo => dac;
+            440 * Math.pow(2.0, {0}/19.0) => foo.freq;
+            samp => now; 
+        ", count));
+    }
+    //called just before performing physics calculations.
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+
+        GetComponent<ChuckSubInstance>().RunCode(string.Format(@"
+            SinOsc foo => dac;
+            repeat(12)
+            {{
+            Math.random2f(440 * Math.pow(2.0, {0}/12.0), 660 * Math.pow(2.0, {0}/12.0)) => foo.freq;
+            25::ms => now; 
+            440 * Math.pow(2.0, {0}/12.0) => foo.freq;
+            25::ms => now;
+            }}        
+        ", count));
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
+
+//Destroy(other.gameObject);
